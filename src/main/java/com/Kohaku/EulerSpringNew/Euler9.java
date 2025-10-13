8e0f8d9c02817d61bdb323b31a086d1cc803c316
Leti-NTT <letizia.cravotta@emeal.nttdata.com>
2025-10-13 14:31:01 +0200

    Add Euler9 + tests

--- src/main/java/com/Kohaku/EulerSpringNew/Euler9.java
+++ src/main/java/com/Kohaku/EulerSpringNew/Euler9.java
@@ -1,5 +1,31 @@
 package com.Kohaku.EulerSpringNew;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Euler9 {
-    //myEuler9Code
+
+    /**
+     * Finds the product a * b * c of the Pythagorean triplet for which a + b + c equals the specified sum.
+     * If no such triplet exists, returns -1.
+     *
+     * @param sumOfABC the target sum for a + b + c
+     * @return the product a * b * c if a valid triplet is found, otherwise -1
+     */
+   public int solve(int sumOfABC){
+       for(int a=1; a<sumOfABC; a++){
+           for(int b=a; b<sumOfABC; b++){
+               int c = sumOfABC - a - b;
+               if(c > 0 && isPythagoreanTriplet(a, b, c)){
+                   return a*b*c;
+               }
+           }
+       }
+       return -1;
+   }
+
+   public boolean isPythagoreanTriplet(int a, int b, int c){
+       return a*a + b*b == c*c;
+   }
+
 }
---
+++ src/test/java/com/Kohaku/EulerSpringNew/Euler9Test.java
@@ -0,0 +1,26 @@
+package com.Kohaku.EulerSpringNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class Euler9Test {
+
+    @Autowired
+    Euler9 euler9;
+
+    @Test
+    void solve(){
+        assertEquals(60, euler9.solve(12));
+        assertEquals(31875000, euler9.solve(1000));
+
+    }
+    @Test
+    void isPythagoreanTriplet(){
+        assertTrue(euler9.isPythagoreanTriplet(3,4,5));
+        assertFalse(euler9.isPythagoreanTriplet(3,4,6));
+    }
+}
